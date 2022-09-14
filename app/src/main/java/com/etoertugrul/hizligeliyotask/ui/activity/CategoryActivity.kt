83cfba382b21e91ee98a1f3384027ee1747eb410
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-09-14 23:39:56 +0300

    created activities

---
+++ app/src/main/java/com/etoertugrul/hizligeliyotask/ui/activity/CategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.etoertugrul.hizligeliyotask.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.etoertugrul.hizligeliyotask.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
---
+++ app/src/main/java/com/etoertugrul/hizligeliyotask/ui/activity/ProductActivity.kt
@@ -0,0 +1,12 @@
+package com.etoertugrul.hizligeliyotask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.etoertugrul.hizligeliyotask.R
+
+class ProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
