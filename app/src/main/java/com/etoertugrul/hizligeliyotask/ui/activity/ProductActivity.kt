182abed9ab8be1a2efd5f4be1fe704abc09c1a66
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-09-15 00:59:32 +0300

    created navigation component.

--- app/src/main/java/com/etoertugrul/hizligeliyotask/ui/activity/ProductActivity.kt
+++ app/src/main/java/com/etoertugrul/hizligeliyotask/ui/activity/ProductActivity.kt
@@ -3,16 +3,31 @@ package com.etoertugrul.hizligeliyotask.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.etoertugrul.hizligeliyotask.R
-import com.etoertugrul.hizligeliyotask.ui.viewmodel.LoginViewModel
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.etoertugrul.hizligeliyotask.databinding.ActivityProductBinding
 import com.etoertugrul.hizligeliyotask.ui.viewmodel.ProductViewModel
 
 class ProductActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product)
+        binding = ActivityProductBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val viewModel: ProductViewModel =
             ViewModelProvider(this)[ProductViewModel::class.java]
+
+        //Setup navigation component
+        binding.apply {
+            val navController = (supportFragmentManager
+                .findFragmentById(fragmentContainer.id) as NavHostFragment)
+                .navController
+            setupActionBarWithNavController(navController)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+
     }
 }
